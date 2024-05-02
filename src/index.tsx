b24636c5637ca2d70b80ab12d446f8990c08379f
bianca-leahu <bya.leahu@gmail.com>
2024-05-02 16:40:13 +0300

    minor

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,19 @@ import App from './App.tsx';
 
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.warn('Root element not found.');
+}
+
+
 
 reportWebVitals();
